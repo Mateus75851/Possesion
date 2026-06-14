e74bd29e490875467c262b19fbc795f702bb5a6a
Mateus Chaves Fonseca <mateusfonseca912@gmail.com>
2026-06-14 17:38:15 -0300

    Criar as estruturas básicas dos viewsets de Campeonato, Clube e Participacao.

--- competicoes/views.py
+++ competicoes/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Campeonato, Clube, Participacao
+from .serializers import CampeonatoSerializer, ClubeSerializer, ParticipacaoSerializer
 
-# Create your views here.
+
+class CampeonatoViewSet(viewsets.ModelViewSet):
+    queryset = Campeonato.objects.all()
+    serializer_class = CampeonatoSerializer
+
+class ClubeViewSet(viewsets.ModelViewSet):
+    queryset = Clube.objects.all()
+    serializer_class = ClubeSerializer
+
+class ParticipacaoViewSet(viewsets.ModelViewSet):
+    queryset = Participacao.objects.all()
+    serializer_class = ParticipacaoSerializer
+
+
