0a1af49ae5c266ed1ac8b86dfaf02b6de072fb48
Mateus Chaves Fonseca <mateusfonseca912@gmail.com>
2026-06-14 22:32:54 -0300

    crie um endpoint que mostre a classificação de um campeonato e um serializer dedicado a ele

--- competicoes/serializers.py
+++ competicoes/serializers.py
@@ -16,3 +16,12 @@ class ParticipacaoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Participacao
         fields = '__all__'
+
+class ClassificacaoSerializer(serializers.ModelSerializer):
+    clube = ClubeSerializer()
+    saldo_de_gols = serializers.IntegerField()
+
+    class Meta:
+        model = Participacao
+        fields = ['clube', 'pontos', 'vitorias', 'empates', 'derrotas', 'gols_feitos', 'gols_sofridos', 'saldo_de_gols']
+        read_only_fields = ['clube', 'pontos', 'vitorias', 'empates', 'derrotas', 'gols_feitos', 'gols_sofridos', 'saldo_de_gols']
--- competicoes/views.py
+++ competicoes/views.py
@@ -1,16 +1,24 @@
+from django.db.models import F
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from .models import Campeonato, Clube, Participacao
-from .serializers import CampeonatoSerializer, ClubeSerializer, ParticipacaoSerializer
+from .serializers import CampeonatoSerializer, ClubeSerializer, ParticipacaoSerializer, ClassificacaoSerializer
 
 
 class CampeonatoViewSet(viewsets.ModelViewSet):
     queryset = Campeonato.objects.all()
     serializer_class = CampeonatoSerializer
 
-    '''@action(detail=True, methods=['get'])
+    @action(detail=True, methods=['get'])
     def classificacao(self, request, pk=None):
         campeonato = self.get_object()
-        participacoes = campeonato.participacoes.all()'''
+        queryset_classificacao = campeonato.participacoes.annotate(
+            saldo_de_gols=F('gols_feitos')-F('gols_sofridos')
+        ).order_by('-pontos', '-vitorias', '-saldo_de_gols', '-gols_feitos')
+        dicionario_classificacao = ClassificacaoSerializer(queryset_classificacao, many=True).data 
+
+        return Response(dicionario_classificacao)
 
 
 
